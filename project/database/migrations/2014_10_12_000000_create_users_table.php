bce3ab7fd0b1dc49a6b9df0ad2037971353ef2d9
Paweł Lech <plech@student.agh.edu.pl>
2023-04-23 22:59:27 +0200

    Modified users table - photo added and listed employees in schedule view for CEO

--- project/database/migrations/2014_10_12_000000_create_users_table.php
+++ project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignIdFor(\App\Models\Location::class);
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
